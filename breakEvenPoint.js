a5fba0e00e5bfd15c01bdc0fffd920332a684a2a
philip1029 <philip091029@gmail.com>
2023-09-28 22:00:41 +0900

    v1.1.1
    심각한 계산 오류 수정

--- breakEvenPoint.js
+++ breakEvenPoint.js
@@ -1,4 +1,4 @@
-function win()  {
+function fun()  {
     /** 고정비용 */
     const a = Number(document.getElementById('a').value);
 
@@ -13,14 +13,18 @@ function win()  {
 
     const a_ = a * 100;
     const b_ = b * 100;
-    const r2_ = Math.floor(r1 * 10000)/100;
+    const r1_ = Math.round(r1 * 10000)/100;
 
-    if(r1 == 0) {r1 = "-"}
     document.getElementById("result1").innerText = String(r1) + "만 원";
 
-    var r2 = Math.round((a_/(r2_ - b_)*100))/100
-    if(a_/(r2_ - b_) == Infinity) {r2 = "-"}
+    const r3_ = Math.round((a_/(r1_ - b_)*100));
+    var r2 = (r1_ * r3_)/10000;
+
+    var r3 = r3_/100;
+
     document.getElementById("result2").innerText = String(r2) + "만 원";
 
-    //console.log(a_, b_, r1, r2, r2_)
+    document.getElementById("result3").innerText = String(r3) + "개";
+
+    //console.log("a_:", a_, "b_:", b_, r1, r3, "r1_:", r1_, "r3_:", r3_)
 }
